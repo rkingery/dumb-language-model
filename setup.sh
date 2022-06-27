b7a8260e13261029193573e5ce19ad40dec596a6
Ryan Kingery <rkinger@g.clemson.edu>
2022-06-27 18:24:03 -0400

    fixing shit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git lfs install
 git clone https://huggingface.co/rkingery/dumb-language-model
-mv dumb-language-model models
+# mv dumb-language-model models
 pip install -r requirements.txt
