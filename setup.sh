2370a6ab4f93c2bc295520f39aa1c6e6e3ad74fd
rkingery <rkinger@g.clemson.edu>
2022-06-27 18:35:28 -0400

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git lfs install
 git clone https://huggingface.co/rkingery/dumb-language-model
-# mv dumb-language-model models
+mv dumb-language-model/* models
+rm -rf dumb-language-model
 pip install -r requirements.txt
